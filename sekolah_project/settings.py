a100497242dda79f9c89beeda03b18c1e447f859
Musa0575 <107524599+Musa0575@users.noreply.github.com>
2026-08-21 09:19:54 +0700

    Update settings.py

--- sekolah_project/settings.py
+++ sekolah_project/settings.py
@@ -74,7 +74,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
